f07e3b2c291e1637d4e6d4cf077fd5474ff03570
Vesa-Matti Virolainen <vesaviro@gmail.com>
2023-10-14 14:08:06 +0300

    Format utils-file

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,10 @@
 export const minutesToTimeString = (minutes: number) => {
-    const durationHours = Math.floor(minutes/60)
+  const durationHours = Math.floor(minutes / 60);
 
-    const durationMinutes = minutes - durationHours*60
+  const durationMinutes = minutes - durationHours * 60;
 
-    // Adds leading zero if necessary
-    const leadingZeroPrefix = durationMinutes < 10 ? "0" : ""
+  // Adds leading zero if necessary
+  const leadingZeroPrefix = durationMinutes < 10 ? "0" : "";
 
-    return `${durationHours}:${leadingZeroPrefix}${durationMinutes}`
-}
+  return `${durationHours}:${leadingZeroPrefix}${durationMinutes}`;
+};
