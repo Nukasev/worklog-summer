de8fd991fda8bb38f77fb2f60c679aaf739909c9
Vesa-Matti Virolainen <vesaviro@gmail.com>
2023-10-19 21:02:24 +0300

    Type fixes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 export const space = " ";
 
 type minuteTens = "0" | "1" | "2" | "3" | "4" | "5";
-type minuteOnes = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
+type minuteOnes = minuteTens | "6" | "7" | "8" | "9";
 type minuteString = `${minuteTens}${minuteOnes}`;
 
 type earlyHours = minuteOnes;
@@ -26,10 +26,6 @@ export const minutesToTimeString = (minutes: number): timeString => {
 
 export const timeStringToMinutes = (timeString: timeString): number => {
   const timeSplit = timeString.split(":");
-
-  if (timeSplit.length !== 2) {
-    //TODO: error handling
-  }
 
   const hours = parseInt(timeSplit[0]);
   const minutes = parseInt(timeSplit[1]);
